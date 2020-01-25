00ee531dc9f04c303c87c6acf11b34cad02e8bb4
Germán Quero <german@localhost.localdomain>
2020-01-25 12:36:20 +0100

    nnnnnnnnnnnn

--- source/scripts.js
+++ source/scripts.js
@@ -14,7 +14,7 @@ function drawButtons(){
         document.querySelector(".compressed").innerHTML += `
         <a class="downloadButton all" href="${jsonData.compressed[i].path}" download="${jsonData.compressed[i].download}">
         <div><img src="images/downloading-file.png" alt="X" class="image downloadIcon"></div>
-        <div class="text fileName">${jsonData.compressed[i].name}</div>
+        <div class="text fileName">${jsonData.compressed[i].name} ${jsonData.compressed[i].size}</div>
         <div class="text weight>${jsonData.compressed[i].size}</div>
         </a>`;
     }
@@ -23,7 +23,7 @@ function drawButtons(){
         document.querySelector(".video").innerHTML += `
         <a class="downloadButton" href="${jsonData.videos[i].path}" download="${jsonData.videos[i].download}">
         <div><img src="images/downloading-file.png" alt="X" class="image downloadIcon"></div>
-        <div class="text fileName">${jsonData.videos[i].name}</div>
+        <div class="text fileName">${jsonData.videos[i].name} ${jsonData.videos[i].size}</div>
         <div class="text weight>${jsonData.videos[i].size}</div>
         </a>`;
     }
