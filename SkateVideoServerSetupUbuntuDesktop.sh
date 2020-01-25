2f921fef9e9f4f7ced1cfe2838e79ebcdab4e95c
German Quero <german.querojimenez@gmail.com>
2020-01-25 04:40:47 +0100

    Keep correcting stuff

--- SkateVideoServerSetupUbuntuDesktop.sh
+++ SkateVideoServerSetupUbuntuDesktop.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+script_dir=$(dirname $0)
+
+if [ $script_dir == "." ]
+	then source_dir="source"
+		content_dir="content"
+else
 source_dir=$(echo $(dirname $0)"/source")
 content_dir=$(echo $(dirname $0)"/content")
+fi
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-    else
+if [ "$EUID" -ne 0 ];then
+    echo "Please run as root"
+else
     while true; do
         read -p "Please make sure you internet conection and enough battery life to proceed. Continue (y/n)?" choice
         case "$choice" in 
@@ -39,8 +46,8 @@ if [ "$EUID" -ne 0 ]
         cp $source_dir/index.html /var/www/html/skatevideoserver/
         cp $source_dir/error.html /var/www/html/skatevideoserver/
         cp $source_dir/master.css /var/www/html/skatevideoserver/css/
-        cp $source_dir/desktop.css /var/www/html/skatevideoserver/css/
-        cp $source_dir/mobile.css /var/www/html/skatevideoserver/css/
+        cp $source_dir/desktops.css /var/www/html/skatevideoserver/css/
+        cp $source_dir/mobiles.css /var/www/html/skatevideoserver/css/
         cp $source_dir/tablets.css /var/www/html/skatevideoserver/css/
         cp $source_dir/scripts.js /var/www/html/skatevideoserver/scripts/
         cp $source_dir/error.js /var/www/html/skatevideoserver/scripts/
@@ -52,8 +59,8 @@ if [ "$EUID" -ne 0 ]
         cp $source_dir/github-logo.png /var/www/html/skatevideoserver/images/
         cp $source_dir/html-coding.png /var/www/html/skatevideoserver/images/
         cp $source_dir/skateboard.png /var/www/html/skatevideoserver/images/
-        cp $source_dir/skateboard_flipped.png /var/www/html/skatevideoserver/images/
-        cp $source_dir/skateboard_flipped_2.png /var/www/html/skatevideoserver/images/
+        cp $source_dir/skateboard_fliped.png /var/www/html/skatevideoserver/images/
+        cp $source_dir/skateboard_fliped_2.png /var/www/html/skatevideoserver/images/
         cp $source_dir/skateboard_2.png /var/www/html/skatevideoserver/images/
         cp $source_dir/skateboard_color.png /var/www/html/skatevideoserver/images/
         ## create symbolic link in home for easier access
@@ -69,13 +76,14 @@ if [ "$EUID" -ne 0 ]
         ln -s $HOME/backend_skatevideoserver/config.sh /usr/local/bin/config
 
         ## put videos and comrpessed files in place
-        mkdir $HOME/content_skatevideoserver/videos $HOME/content_skatevideoserver/compressed
+        mkdir $HOME/content_skatevideoserver
+	mkdir $HOME/content_skatevideoserver/videos $HOME/content_skatevideoserver/compressed
         cp $content_dir/* $HOME/content_skatevideoserver/videos/
 
         ## configure server apache-config files
-        cp $source_dir/skatevideoserver.conf /etc/apache2/sites-aviable/
+        cp $source_dir/skatevideoserver.conf /etc/apache2/sites-available/
         rm /etc/apache2/sites-enabled/*
-        ln -s /etc/apahce2/sites-aviable/skatevideoserver.conf /etc/apache2/sites-enabled/skatevideoserver.conf
+        ln -s /etc/apahce2/sites-available/skatevideoserver.conf /etc/apache2/sites-enabled/skatevideoserver.conf
 
 
         ## set custom wallpaper
@@ -83,8 +91,11 @@ if [ "$EUID" -ne 0 ]
         mkdir $HOME/.wallpaper
         cp $source_dir/wallpaper.png $HOME/.wallpaper
         
+	## start services
+	service apache2 start
+
         ## demo of web page
-        config.sh --assume-yes
+        config --assume-yes
         cat $source_dir/manual.txt
         cp $source_dir/manual.txt $HOME/
     
