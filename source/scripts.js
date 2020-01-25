103de66f9f75db5ac2083747d79b2f279ba42cff
Germán Quero <german@localhost.localdomain>
2020-01-25 12:40:21 +0100

    mmmmmmmm

--- source/scripts.js
+++ source/scripts.js
@@ -10,21 +10,19 @@ function checkJson(){
 
 function drawButtons(){
     for(var i = 0; i < jsonData.compressed.length; ++i){
-        console.log(jsonData.compressed[i].size);
         document.querySelector(".compressed").innerHTML += `
         <a class="downloadButton all" href="${jsonData.compressed[i].path}" download="${jsonData.compressed[i].download}">
         <div><img src="images/downloading-file.png" alt="X" class="image downloadIcon"></div>
-        <div class="text fileName">${jsonData.compressed[i].name} ${jsonData.compressed[i].size}</div>
-        <div class="text weight>${jsonData.compressed[i].size}</div>
+        <div class="text fileName">${jsonData.compressed[i].name}</div>
+        <div class="text weight">${jsonData.compressed[i].size}</div>
         </a>`;
     }
     for(var i = 0; i < jsonData.videos.length; ++i){
-        console.log(jsonData.videos[i].size);
         document.querySelector(".video").innerHTML += `
         <a class="downloadButton" href="${jsonData.videos[i].path}" download="${jsonData.videos[i].download}">
         <div><img src="images/downloading-file.png" alt="X" class="image downloadIcon"></div>
-        <div class="text fileName">${jsonData.videos[i].name} ${jsonData.videos[i].size}</div>
-        <div class="text weight>${jsonData.videos[i].size}</div>
+        <div class="text fileName">${jsonData.videos[i].name}</div>
+        <div class="text weightW>${jsonData.videos[i].size}</div>
         </a>`;
     }
 }
