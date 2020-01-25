3875bc81ed1b2dbb16be32ec1927ec5b939ab96b
Germán Quero <german@localhost.localdomain>
2020-01-25 10:53:09 +0100

    AKSH

--- source/error.js
+++ source/error.js
@@ -6,4 +6,6 @@ function comeBack(){
     if(jsonData != undefined && jsonData != null && jsonData.compressed != undefined && jsonData.compressed != null && jsonData.videos != undefined && jsonData.videos != null){
         window.location.replace("index.html");
     }
+    else
+        console.log(jsonData);
 }
